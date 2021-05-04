132f920306569cbd0f9a21063024b52daff1e643
StacyIko <9huston@mail.ru>
2021-05-04 15:45:16 +0200

    added all sections

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const svgSprite = require('gulp-svg-sprite');
+const	cheerio = require('gulp-cheerio');
+const newer = require('gulp-newer');
 // const fileinclude = require('gulp-file-include');
 
 function browsersync() {
@@ -29,13 +32,14 @@ function styles() {
 		.pipe(browserSync.stream())
 }
 
+
 function scripts() {
 	return src([
 		'node_modules/slick-carousel/slick/slick.js',
-		// 'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 		// 'node_modules/mixitup/dist/mixitup.min.js',
 		// 'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-		// 'node_modules/rateyo/src/jquery.rateyo.js',
+		'node_modules/rateyo/src/jquery.rateyo.js',
 		// 'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
 		'app/js/main.js'
 	])
@@ -47,6 +51,7 @@ function scripts() {
 
 function images() {
 	return src('app/images/**/*.*')
+	.pipe(newer('dist/images'))
 	.pipe(imagemin([
     imagemin.gifsicle({interlaced: true}),
     imagemin.mozjpeg({quality: 75, progressive: true}),
@@ -61,14 +66,38 @@ function images() {
 	.pipe(dest('dist/images'))
 }
 
-function build() {
-	return src([
-		'app/**/*.html',
-		'app/css/style.min.css',
-		'app/js/main.min.js'
-	], {base: 'app'})
-	.pipe(dest('dist'))
+function svgSprites() {
+	return src('app/images/sprite/**.svg')
+		.pipe(svgSprite({
+			mode: {
+				stack: {
+					sprite: '../sprite.svg',
+					render: {
+						scss: {
+							dest:'../../scss/_sprite.scss',
+							// template: '../../scss/templates/_sprite_template.scss'
+						}
+					}
+				}
+			}
+		}))
+
+		.pipe(dest('app/images'))
 }
+
+function cheerioFunc() {
+		return src('app/images/sprite/**.svg')
+			.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill');
+				$('[stroke]').removeAttr('stroke');
+				$('[style]').removeAttr('style');
+			},
+			parserOptions: {xmlMode: true}
+		}))
+		.pipe(dest('app/images/sprite'))
+}
+
 
 function cleanDist() {
 	return del('dist');
@@ -86,7 +115,9 @@ function cleanDist() {
 
 function watching() {
 	watch(['app/scss/**/*.scss'], styles);
-	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
+	watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts),
+	// watch(['app/images/sprite/**.svg'], cheerioFunc),
+	watch(['app/images/sprite/**.svg'], svgSprites),
 	// watch(['app/html/**/*.html'], includeHtml)
 	watch(['app/**/**/*.html']).on('change', browserSync.reload)
 }
@@ -96,8 +127,21 @@ exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
+exports.cheerioFunc = cheerioFunc;
 // exports.includeHtml = includeHtml;
-exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(styles, scripts, cheerioFunc, svgSprites, browsersync, watching);
+
+
+function build() {
+	return src([
+		'app/**/*.html',
+		'app/css/style.min.css',
+		'app/js/main.min.js'
+	], {base: 'app'})
+	.pipe(dest('dist'))
+}
+
+exports.build = series(cleanDist, parallel(styles, scripts, cheerioFunc, svgSprites), images, build);
